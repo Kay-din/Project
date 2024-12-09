e0b2d8822f7a0ff72c2f0a632a223992e4900c10
Kay-din <tut35627@temple.edu>
2024-12-09 16:58:01 -0500

    Unity building project

--- Myproject/Assets/Scripts/Movement/PlayerControls.cs
+++ Myproject/Assets/Scripts/Movement/PlayerControls.cs
@@ -12,6 +12,9 @@ public class PlayerController : MonoBehaviour
     
     private Animator animator;
 
+    public LayerMask soLidObjectsLayer;
+    public LayerMask interactbleLayer;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -23,8 +26,8 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
-            Debug.Log("This is input.x" + input.x);
-            Debug.Log("This is input.y" + input.y);
+            //Debug.Log("This is input.x" + input.x);
+            //Debug.Log("This is input.y" + input.y);
 
 
             if (input.x != 0) input.y = 0;
@@ -34,15 +37,38 @@ public class PlayerController : MonoBehaviour
                 animator.SetFloat("MoveX", input.x);
                 animator.SetFloat("MoveY", input.y);
                 
-                var targetPos = transform.position;
-                targetPos.x += input.x;
-                targetPos.y += input.y;
+                //var targetPos = transform.position;
+                //targetPos.x += input.x;
+                //targetPos.y += input.y;
+                Vector3 targetPos = transform.position;
+                targetPos.x += input.x * moveSpeed * Time.deltaTime;
+                targetPos.y += input.y * moveSpeed * Time.deltaTime;
 
-                StartCoroutine(Move(targetPos));
+                if (isWalkable(targetPos))
+                    StartCoroutine(Move(targetPos));
             }
         }
 
         animator.SetBool("isMoving", isMoving);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+            Interact();
+        
+    }
+
+    void Interact()
+    {
+        var facingDir = new Vector3(animator.GetFloat("MoveX"), animator.GetFloat("MoveY"));
+        var interactPos = transform.position + facingDir;
+
+        Debug.DrawLine(transform.position,interactPos, Color.red, 1f);
+
+        var collider = Physics2D.OverlapCircle(interactPos,0.2f,interactbleLayer);
+        if (collider != null)
+        {
+            Debug.Log("There is an interable here.");
+        }
+
     }
 
     IEnumerator Move(Vector3 targetPos)
@@ -56,5 +82,14 @@ public class PlayerController : MonoBehaviour
         transform.position = targetPos;
 
         isMoving = false;
+    }
+
+    private bool isWalkable(Vector3 targetPos)
+    {
+        if(Physics2D.OverlapCircle(targetPos,0.2f, soLidObjectsLayer | interactbleLayer) != null)
+        {
+            return false;
+        }
+        return true;
     }
 }
